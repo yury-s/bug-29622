91f605be2b92b8b2ab64456e86ad5b4299d8d10a
Yury Semikhatsky <yurys@chromium.org>
2024-02-22 12:07:10 -0800

    Initial

---
+++ tests/example.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+function helper() {
+  throw new Error('This is a helper function');
+}
+
+test.beforeAll(() => {
+  helper();
+});
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
